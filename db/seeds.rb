905724ec462833a8e3d7bce741e3d083f95c3919
Sumit Birla <sbirla@tampahost.net>
2014-08-23 23:34:19 -0400

    updated gemspec

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+Permission.create(section: 'cms', resource: 'admin', action: 'login')
+
+Permission.create(section: 'cms', resource: 'page', action: 'create')
+Permission.create(section: 'cms', resource: 'page', action: 'update')
+Permission.create(section: 'cms', resource: 'page', action: 'destroy')
+
+Permission.create(section: 'cms', resource: 'content_block', action: 'create')
+Permission.create(section: 'cms', resource: 'content_block', action: 'update')
+Permission.create(section: 'cms', resource: 'content_block', action: 'destroy')
+
+Permission.create(section: 'cms', resource: 'menu', action: 'create')
+Permission.create(section: 'cms', resource: 'menu', action: 'update')
+Permission.create(section: 'cms', resource: 'menu', action: 'destroy')
+
+Permission.create(section: 'cms', resource: 'location', action: 'read')
+Permission.create(section: 'cms', resource: 'location', action: 'create')
+Permission.create(section: 'cms', resource: 'location', action: 'update')
+Permission.create(section: 'cms', resource: 'location', action: 'destroy')
+
+Permission.create(section: 'cms', resource: 'photo_album', action: 'read')
+Permission.create(section: 'cms', resource: 'photo_album', action: 'create')
+Permission.create(section: 'cms', resource: 'photo_album', action: 'update')
+Permission.create(section: 'cms', resource: 'photo_album', action: 'destroy')
+
+Permission.create(section: 'cms', resource: 'comment', action: 'read')
+Permission.create(section: 'cms', resource: 'comment', action: 'create')
+Permission.create(section: 'cms', resource: 'comment', action: 'update')
+Permission.create(section: 'cms', resource: 'comment', action: 'destroy')
+Permission.create(section: 'cms', resource: 'comment', action: 'approve')
--- rhombus_cms.gemspec
+++ rhombus_cms.gemspec
@@ -7,8 +7,8 @@ require "rhombus_cms/version"
 Gem::Specification.new do |s|
   s.name        = "rhombus_cms"
   s.version     = RhombusCms::VERSION
-  s.authors     = ["TODO: Your name"]
-  s.email       = ["TODO: Your email"]
+  s.authors     = ["Sumit Birla"]
+  s.email       = ["sbirla@tampahost.net"]
   s.homepage    = "TODO"
   s.summary     = "TODO: Summary of RhombusCms."
   s.description = "TODO: Description of RhombusCms."
