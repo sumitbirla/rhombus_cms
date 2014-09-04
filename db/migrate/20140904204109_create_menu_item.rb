d19b492d2c5adc75d9461885323f908a0d385e89
Sumit Birla <sbirla@tampahost.net>
2014-09-04 16:43:06 -0400

    added menu_item

---
+++ db/migrate/20140904204109_create_menu_item.rb
@@ -0,0 +1,12 @@
+class CreateMenuItem < ActiveRecord::Migration
+  def change
+    create_table :cms_enu_items do |t|
+      t.references :menu, index: true
+      t.string :title, null: false
+      t.string :link_url, null: false
+      t.integer :sort, null: false
+      t.boolean :enabled, null: false, default: true
+	  t.timestamps
+    end
+  end
+end
