3cb1324e46fc99c113d8e8787a599359335c9f95
Sumit Birla <sbirla@tampahost.net>
2019-08-16 21:37:06 +0200

    fix  attachment.set_md5

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -8,7 +8,7 @@ class Attachment < ApplicationRecord
   validates_presence_of :file_path, :content_type
   
   def set_md5
-    dir = System.setting(Rails.configuration.domain_id, :system, "Static Files Path")
+    dir = Setting.get(Rails.configuration.domain_id, :system, "Static Files Path")
     self.md5 = Digest::MD5.hexdigest(File.read(dir + file_path))
   end
 end
